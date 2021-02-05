3856ed466b249f134abe014ae37649cf64dff501
LeoDeg <leodeg.dev@gmail.com>
2021-02-05 21:26:50 +0300

    feat: add swagger xml comments

--- TweetBook/TweetBook/Installers/Concrete/MVCInstaller.cs
+++ TweetBook/TweetBook/Installers/Concrete/MVCInstaller.cs
@@ -9,7 +9,6 @@ using TweetBook.Filters;
 
 namespace TweetBook.Installers
 {
-
 	public class MVCInstaller : IInstaller
 	{
 		public void InstallServices(IServiceCollection services, IConfiguration configuration)
@@ -23,60 +22,6 @@ namespace TweetBook.Installers
 				.AddFluentValidation(configuration =>
 					configuration.RegisterValidatorsFromAssemblyContaining<Startup>())
 				.SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0);
-
-			services.AddSwaggerGen(option =>
-			{
-				option.SwaggerDoc("v1", new OpenApiInfo
-				{
-					Version = "v1",
-					Title = "TweetBook API",
-					Description = "A simple example ASP.NET Core REST API",
-					Contact = new OpenApiContact
-					{
-						Name = "LeoDeg",
-						Email = string.Empty,
-						Url = new Uri("https://github.com/leodeg"),
-					}
-				});
-
-				var security = new Dictionary<string, IEnumerable<string>>
-				{
-					{ "Bearer", new string[0]}
-				};
-
-				option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-				{
-					Description =
-						"JWT Authorization header using the Bearer scheme. \r\n\r\n " +
-						"Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
-						"Example: \"Bearer 12345abcdef\"",
-					Name = "Authorization",
-					In = ParameterLocation.Header,
-					Type = SecuritySchemeType.ApiKey,
-					Scheme = "Bearer"
-				});
-
-				option.AddSecurityRequirement(new OpenApiSecurityRequirement
-				{
-					{
-						new OpenApiSecurityScheme
-						{
-							Reference = new OpenApiReference
-							{
-								Type = ReferenceType.SecurityScheme,
-								Id = "Bearer"
-							},
-							Scheme = "oauth2",
-							Name = "Bearer",
-							In = ParameterLocation.Header,
-
-						},
-						new List<string>()
-					}
-				});
-
-				option.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
-			});
 		}
 	}
 }
---
+++ TweetBook/TweetBook/Installers/Concrete/SwaggerInstaller.cs
@@ -0,0 +1,72 @@
+﻿using FluentValidation.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using TweetBook.Filters;
+
+namespace TweetBook.Installers
+{
+	public class SwaggerInstaller : IInstaller
+	{
+		public void InstallServices(IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddSwaggerGen(option =>
+			{
+				option.SwaggerDoc("v1", new OpenApiInfo
+				{
+					Version = "v1",
+					Title = "TweetBook API",
+					Description = "A simple example ASP.NET Core REST API",
+					Contact = new OpenApiContact
+					{
+						Name = "LeoDeg",
+						Email = string.Empty,
+						Url = new Uri("https://github.com/leodeg"),
+					}
+				});
+
+				option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+				{
+					Description =
+						"JWT Authorization header using the Bearer scheme. \r\n\r\n " +
+						"Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n" +
+						"Example: \"Bearer 12345abcdef\"",
+					Name = "Authorization",
+					In = ParameterLocation.Header,
+					Type = SecuritySchemeType.ApiKey,
+					Scheme = "Bearer"
+				});
+
+				option.AddSecurityRequirement(new OpenApiSecurityRequirement
+				{
+					{
+						new OpenApiSecurityScheme
+						{
+							Reference = new OpenApiReference
+							{
+								Type = ReferenceType.SecurityScheme,
+								Id = "Bearer"
+							},
+							Scheme = "oauth2",
+							Name = "Bearer",
+							In = ParameterLocation.Header,
+
+						},
+						new List<string>()
+					}
+				});
+
+				var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+				var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+				option.IncludeXmlComments(xmlPath);
+
+				option.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+			});
+		}
+	}
+}
